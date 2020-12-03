cf5d9340a4b27ffe15565c013cd645930b9aabfd
patrickmylius <patrickmylius@outlook.dk>
2020-12-03 11:00:57 +0100

    Succesfully fixed style.css to hold image and create text colors etc.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,15 +4,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
+
+import java.awt.*;
+import java.io.FileInputStream;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml")); //loads sample.fxml file
+
         primaryStage.setTitle("Booty Call A Legendary 1.0"); //Set window name
-        primaryStage.setScene(new Scene(root, 1280, 720)); //Set scene and size.
+        primaryStage.setScene(new Scene(root, 960, 680)); //Set scene and size.
         primaryStage.show();//Showing primary Stage
     }
 
