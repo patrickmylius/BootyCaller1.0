65760de384abd9a7e467b051eda56a513e88f8a0
patrickmylius <patrickmylius@outlook.dk>
2020-12-03 16:33:29 +0100

    Death knight generate legendary function works.
    Trying to figure how to manage control of the items in specChoiceBox, without having to create ChoiceBox in Controller.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,6 +3,7 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -21,13 +22,12 @@ public class Controller {
     @FXML
     private Label roleLabel;
     @FXML
-    private TextField roleField;
+    private ChoiceBox classChoice;
 
     @FXML
     private Label specLabel;
     @FXML
-    private TextField specField;
-
+    private ChoiceBox specChoice;
     @FXML
     private Button clearButton;
     @FXML
@@ -37,16 +37,64 @@ public class Controller {
     //create method for handling clickClearButton event
     @FXML
     protected void handleClearButtonAction(ActionEvent event) {
-
+        image.setImage(null);
     }
 
     //create method for handling click generateButton event
     @FXML
     protected void handleGenerateButtonAction(ActionEvent event) {
-        switch (specField.getText().toLowerCase()) {
-            case "unholy":
-                Image unholy = new Image("resources/" + "Unholy.png");
-                image.setImage(unholy);
+        String userClass = (String)classChoice.getValue();
+        String userSpec = (String)specChoice.getValue();
+
+        switch (userClass.toLowerCase()) {
+            //CLASS: Death Knight
+            case "death knight":
+                switch (userSpec.toLowerCase()) {
+                    case "unholy":
+                        Image unholy = new Image("resources/UnholyDK.png");
+                        image.setImage(unholy);
+                        break;
+                    case "frost":
+                        Image frost = new Image("resources/FrostDK.png");
+                        image.setImage(frost);
+                        break;
+                    case "blood":
+                        Image blood = new Image("resources/BloodDK.png");
+                        image.setImage(blood);
+                }
+                break;
+            //CLASS: Mage
+            case "mage":
+                switch (userSpec.toLowerCase()) {
+                    case "fire":
+                        Image fire = new Image("resources/FireMage.png");
+                        image.setImage(fire);
+                        break;
+                    case "frost":
+                        Image frost = new Image("resources/FrostMage.png");
+                        image.setImage(frost);
+                        break;
+                    case "arcane":
+                        Image arcane = new Image("resources/ArcaneMage.png");
+                        image.setImage(arcane);
+                }
+                break;
+            //CLASS: Rogue
+            case "rogue":
+                switch (userSpec.toLowerCase()) {
+                    case "assasination":
+                        Image unholy = new Image("resources/AssaRogue.png");
+                        image.setImage(unholy);
+                        break;
+                    case "subtlety":
+                        Image frost = new Image("resources/SubRogue.png");
+                        image.setImage(frost);
+                        break;
+                    case "outlaw":
+                        Image blood = new Image("resources/OutlawRogue.png");
+                        image.setImage(blood);
+                }
+                break;
         }
 
     }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,12 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.layout.*;
 import javafx.stage.Stage;
-
-import java.awt.*;
-import java.io.FileInputStream;
 
 public class Main extends Application {
 
