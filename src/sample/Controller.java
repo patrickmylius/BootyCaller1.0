11e3ef9b044094b311106e3288e46310578ebb35
patrickmylius <patrickmylius@outlook.dk>
2020-12-03 11:42:05 +0100

    Trying to implement getting fields.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,16 +2,49 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
+import javafx.stage.Window;
+
 
 public class Controller {
-    //create Text
     @FXML
-    private Text actiontarget;
-    //create method for handling click event
-    @FXML
-    protected void handleSubmitButtonAction(ActionEvent event) {
-        actiontarget.setText("Button clicked");
+    private ImageView image;
 
+    @FXML
+    private Label nameLabel;
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private Label roleLabel;
+    @FXML
+    private TextField roleField;
+
+    @FXML
+    private Label specLabel;
+    @FXML
+    private TextField specField;
+
+    @FXML
+    private Button clearButton;
+    @FXML
+    private Button generateButton;
+
+
+    //create method for handling clickClearButton event
+    @FXML
+    protected void handleClearButtonAction(ActionEvent event) {
+
+    }
+
+    //create method for handling click generateButton event
+    @FXML
+    protected void handleGenerateButtonAction(ActionEvent event) {
+        System.out.println(nameField.getText());
     }
 }
