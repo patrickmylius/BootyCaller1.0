8849bd3fd6a84ca1b7ab27c6ab6f5f71f29f5592
patrickmylius <patrickmylius@outlook.dk>
2020-12-04 10:19:31 +0100

    ChoiceBoxes implemented with correct use.
    Added listener to Class choiceBox in sample.fxml
    
    Next: implemented handler for the listener on value gotten from Class choice.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -41,10 +41,59 @@ public class Controller {
     }
 
     @FXML
-    protected void handleSpec(ActionEvent event) {
-        System.out.println("logged: " + classChoice.getValue());
-        specChoice.getItems().clear();
-        specChoice.getItems().addAll("Frost", "Fire", "Arcane");
+    protected void handleSpecValues(ActionEvent event) {
+        String userClass = (String) classChoice.getValue();
+        System.out.println("Class Choice Log: " + classChoice.getValue());
+        switch (userClass.toLowerCase()) {
+            case "death knight":
+                specChoice.getItems().clear();
+                specChoice.getItems().addAll("Frost", "Unholy", "Blood");
+                break;
+            case "mage":
+                specChoice.getItems().clear();
+                specChoice.getItems().addAll("Frost", "Fire", "Arcane");
+                break;
+            case "rogue":
+                specChoice.getItems().clear();
+                specChoice.getItems().addAll("Assasination", "Outlaw", "Subtlety");
+                break;
+            case "warrior":
+                specChoice.getItems().clear();
+                specChoice.getItems().addAll("Arms", "Fury", "Protection");
+                break;
+            case "hunter":
+                specChoice.getItems().clear();
+                specChoice.getItems().addAll("Beast Mastery", "Markmanship", "Survival");
+                break;
+            case "paladin":
+                specChoice.getItems().clear();
+                specChoice.getItems().addAll("Holy", "Protection", "Retribution");
+                break;
+            case "shaman":
+                specChoice.getItems().clear();
+                specChoice.getItems().addAll("Restoration", "Elemental", "Enhancement");
+                break;
+            case "priest":
+                specChoice.getItems().clear();
+                specChoice.getItems().addAll("Shadow", "Holy", "Discipline");
+                break;
+            case "warlock":
+                specChoice.getItems().clear();
+                specChoice.getItems().addAll("Demonology", "Affliction", "Destruction");
+                break;
+            case "monk":
+                specChoice.getItems().clear();
+                specChoice.getItems().addAll("Mistweaver", "Brewmaster", "Windwalker");
+                break;
+            case "demon hunter":
+                specChoice.getItems().clear();
+                specChoice.getItems().addAll("Havoc", "Vengeance");
+                break;
+            case "druid":
+                specChoice.getItems().clear();
+                specChoice.getItems().addAll("Feral", "Balance", "Restoration", "Guardian");
+        }
+
 
     }
 
@@ -55,7 +104,7 @@ public class Controller {
         String userSpec = (String) specChoice.getValue();
 
         switch (userClass.toLowerCase()) {
-            //CLASS: Death Knight
+            //CLASS: DEATH KNIGHT
             case "death knight":
                 switch (userSpec.toLowerCase()) {
                     case "unholy":
@@ -71,7 +120,7 @@ public class Controller {
                         image.setImage(blood);
                 }
                 break;
-            //CLASS: Mage
+            //CLASS: MAGE
             case "mage":
                 switch (userSpec.toLowerCase()) {
                     case "fire":
@@ -87,7 +136,7 @@ public class Controller {
                         image.setImage(arcane);
                 }
                 break;
-            //CLASS: Rogue
+            //CLASS: ROGUE
             case "rogue":
                 switch (userSpec.toLowerCase()) {
                     case "assasination":
@@ -101,6 +150,159 @@ public class Controller {
                     case "outlaw":
                         Image blood = new Image("resources/OutlawRogue.png");
                         image.setImage(blood);
+                }
+                break;
+
+            //CLASS: WARRIOR
+            case "warrior":
+                switch (userSpec.toLowerCase()) {
+                    case "arms":
+                        Image unholy = new Image("resources/ArmsWarrior.png");
+                        image.setImage(unholy);
+                        break;
+                    case "fury":
+                        Image frost = new Image("resources/FuryWarrior.png");
+                        image.setImage(frost);
+                        break;
+                    case "protection":
+                        Image blood = new Image("resources/ProtWarrior.png");
+                        image.setImage(blood);
+                }
+                break;
+
+            //CLASS: HUNTER
+            case "hunter":
+                switch (userSpec.toLowerCase()) {
+                    case "beast mastery":
+                        Image unholy = new Image("resources/BeastHunter.png");
+                        image.setImage(unholy);
+                        break;
+                    case "markmanship":
+                        Image frost = new Image("resources/MMHunter.png");
+                        image.setImage(frost);
+                        break;
+                    case "survival":
+                        Image blood = new Image("resources/SurvHunter.png");
+                        image.setImage(blood);
+                }
+                break;
+
+            //CLASS: PALADIN
+            case "paladin":
+                switch (userSpec.toLowerCase()) {
+                    case "holy":
+                        Image unholy = new Image("resources/HolyPala.png");
+                        image.setImage(unholy);
+                        break;
+                    case "protection":
+                        Image frost = new Image("resources/ProtPala.png");
+                        image.setImage(frost);
+                        break;
+                    case "retribution":
+                        Image blood = new Image("resources/RetPala.png");
+                        image.setImage(blood);
+                }
+                break;
+
+            //CLASS: SHAMAN
+            case "shaman":
+                switch (userSpec.toLowerCase()) {
+                    case "restoration":
+                        Image unholy = new Image("resources/RestoShaman.png");
+                        image.setImage(unholy);
+                        break;
+                    case "elemental":
+                        Image frost = new Image("resources/EleShaman.png");
+                        image.setImage(frost);
+                        break;
+                    case "enhancement":
+                        Image blood = new Image("resources/EnhanceShaman.png");
+                        image.setImage(blood);
+                }
+                break;
+
+            //CLASS: PRIEST
+            case "priest":
+                switch (userSpec.toLowerCase()) {
+                    case "shadow":
+                        Image unholy = new Image("resources/ShadowPriest.png");
+                        image.setImage(unholy);
+                        break;
+                    case "holy":
+                        Image frost = new Image("resources/HolyPriest.png");
+                        image.setImage(frost);
+                        break;
+                    case "discipline":
+                        Image blood = new Image("resources/DiscPriest.png");
+                        image.setImage(blood);
+                }
+                break;
+
+            //CLASS: WARLOCK
+            case "warlock":
+                switch (userSpec.toLowerCase()) {
+                    case "demonology":
+                        Image unholy = new Image("resources/DemoLock.png");
+                        image.setImage(unholy);
+                        break;
+                    case "affliction":
+                        Image frost = new Image("resources/AffLock.png");
+                        image.setImage(frost);
+                        break;
+                    case "destruction":
+                        Image blood = new Image("resources/DestruLock.png");
+                        image.setImage(blood);
+                }
+                break;
+
+            //CLASS: MONK
+            case "monk":
+                switch (userSpec.toLowerCase()) {
+                    case "mistweaver":
+                        Image unholy = new Image("resources/MistMonk.png");
+                        image.setImage(unholy);
+                        break;
+                    case "brewmaster":
+                        Image frost = new Image("resources/BrewMonk.png");
+                        image.setImage(frost);
+                        break;
+                    case "windwalker":
+                        Image blood = new Image("resources/WindMonk.png");
+                        image.setImage(blood);
+                }
+                break;
+
+            //CLASS: DEMON HUNTER
+            case "demon hunter":
+                switch (userSpec.toLowerCase()) {
+                    case "havoc":
+                        Image unholy = new Image("resources/HavocDH.png");
+                        image.setImage(unholy);
+                        break;
+                    case "vengeance":
+                        Image frost = new Image("resources/VengeanceDH.png");
+                        image.setImage(frost);
+                }
+                break;
+
+            //CLASS: DRUID
+            case "druid":
+                switch (userSpec.toLowerCase()) {
+                    case "feral":
+                        Image unholy = new Image("resources/FeralDruid.png");
+                        image.setImage(unholy);
+                        break;
+                    case "balance":
+                        Image frost = new Image("resources/BalanceDruid.png");
+                        image.setImage(frost);
+                        break;
+                    case "restoration":
+                        Image blood = new Image("resources/RestoDruid.png");
+                        image.setImage(blood);
+                        break;
+                    case "guardian":
+                        Image guardian = new Image("resources/GuardianDruid.png");
+                        image.setImage(guardian);
                 }
                 break;
         }
